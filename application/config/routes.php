c39ccba32d61f2ff57d557d42f9cfe13570b11a5
limbermuffin <limbermuffin@gmail.com>
2016-10-01 23:49:23 -0700

    corrected routing for ? link

--- application/config/routes.php
+++ application/config/routes.php
@@ -46,11 +46,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | When you set this option to TRUE, it will replace ALL dashes in the
 | controller and method URI segments.
 |
-| Examples:	my-controller/index	-> my_controller/index
-|		my-controller/my-method	-> my_controller/my_method
+| Examples: my-controller/index -> my_controller/index
+|       my-controller/my-method -> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['sleep'] = 'Sleep/zzz';
 $route['lock/(:any)/(:any)'] = 'Welcome/shucks';
+$route['dunno'] = function() {
+    $source = './data/surprise.jpg';
+    header('Content-type: image/jpeg');
+    header('Content-Disposition: inline');
+    readfile($source);
+    die();
+};
